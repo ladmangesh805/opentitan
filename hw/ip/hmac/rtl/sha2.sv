f4126a4a2eab29f6621aced2bcfd912149ebdf2d
ladmangesh805 <57518512+ladmangesh805@users.noreply.github.com>
2020-04-05 18:04:19 +0530

    Update Sha2 with Sha3 .
    As Sha3 is more faster than Sha2 on hardware and also more secure.

--- hw/ip/hmac/rtl/sha2.sv
+++ hw/ip/hmac/rtl/sha2.sv
@@ -1,315 +1,202 @@
-// Copyright lowRISC contributors.
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-//
-// SHA-256 algorithm
-//
-
-module sha2 import hmac_pkg::*; (
-  input clk_i,
-  input rst_ni,
-
-  input            wipe_secret,
-  input sha_word_t wipe_v,
-
-  // FIFO read signal
-  input             fifo_rvalid,
-  input  sha_fifo_t fifo_rdata,
-  output logic      fifo_rready,
-
-  // Control signals
-  input        sha_en,   // If disabled, it clears internal content.
-  input        hash_start,
-  input        hash_process,
-  output logic hash_done,
-
-  input        [63:0] message_length,   // bits but byte based
-  output sha_word_t [7:0] digest
-);
-
-  logic msg_feed_complete;
-
-  logic      shaf_rready;
-  sha_word_t shaf_rdata;
-  logic      shaf_rvalid;
-
-  logic [$clog2(NumRound)-1:0] round;
-
-  logic      [3:0]  w_index;
-  sha_word_t [15:0] w;
-
-  // w, hash, digest update logic control signals
-  logic update_w_from_fifo, calculate_next_w;
-  logic init_hash, run_hash, complete_one_chunk;
-  logic update_digest, clear_digest;
-
-  logic hash_done_next; // to meet the phase with digest value.
-
-  sha_word_t [7:0] hash;    // a,b,c,d,e,f,g,h
-
-  // Fill up w
-  always_ff @(posedge clk_i or negedge rst_ni) begin : fill_w
-    if (!rst_ni) begin
-      w <= '0;
-    end else if (wipe_secret) begin
-      w <= w ^ {16{wipe_v}};
-    end else if (!sha_en) begin
-      w <= '0;
-    end else if (!run_hash && update_w_from_fifo) begin
-      // this logic runs at the first stage of SHA.
-      w <= {shaf_rdata, w[15:1]};
-    end else if (calculate_next_w) begin
-      w <= {calc_w(w[0], w[1], w[9], w[14]), w[15:1]};
-    //end else if (run_hash && update_w_from_fifo) begin
-    //  // This code runs when round is in [48, 63]. At this time, it reads from the fifo
-    //  // to fill the register if available. If FIFO goes to empty, w_index doesn't increase
-    //  // and it cannot reach 15. Then the sha engine doesn't start, which introduces latency.
-    //  //
-    //  // But in this case, still w should be shifted to feed SHA compress engine. Then
-    //  // fifo_rdata should be inserted in the middle of w index.
-    //  // w[64-round + w_index] <= fifo_rdata;
-    //  for (int i = 0 ; i < 16 ; i++) begin
-    //    if (i == (64 - round + w_index)) begin
-    //      w[i] <= shaf_rdata;
-    //    end else if (i == 15) begin
-    //      w[i] <= '0;
-    //    end else begin
-    //      w[i] <= w[i+1];
-    //    end
-    //  end
-    end else if (run_hash) begin
-      // Just shift-out. There's no incoming data
-      w <= {sha_word_t'(0), w[15:1]};
-    end
-  end : fill_w
-
-  // Update engine
-  always_ff @(posedge clk_i or negedge rst_ni) begin : compress_round
-    if (!rst_ni) begin
-      hash <= '{default:'0};
-    end else if (wipe_secret) begin
-      for (int i = 0 ; i < 8 ; i++) begin
-        hash[i] <= hash[i] ^ wipe_v;
-      end
-    end else if (init_hash) begin
-      hash <= digest;
-    end else if (run_hash) begin
-      hash <= compress( w[0], CubicRootPrime[round], hash);
-    end
-  end : compress_round
-
-  // Digest
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      digest <= '{default: '0};
-    end else if (wipe_secret) begin
-      for (int i = 0 ; i < 8 ; i++) begin
-        digest[i] <= digest[i] ^ wipe_v;
-      end
-    end else if (hash_start) begin
-      for (int i = 0 ; i < 8 ; i++) begin
-        digest[i] <= InitHash[i];
-      end
-    end else if (!sha_en || clear_digest) begin
-      digest <= '0;
-    end else if (update_digest) begin
-      for (int i = 0 ; i < 8 ; i++) begin
-        digest[i] <= digest[i] + hash[i];
-      end
-    end
-  end
-
-  // round
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      round <= '0;
-    end else if (!sha_en) begin
-      round <= '0;
-    end else if (run_hash) begin
-      if (round == (NumRound-1)) begin
-        round <= '0;
-      end else begin
-        round <= round + 1;
-      end
-    end
-  end
-
-  // w_index
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      w_index <= '0;
-    end else if (!sha_en) begin
-      w_index <= '0;
-    end else if (update_w_from_fifo) begin
-      w_index <= w_index + 1;
-    end
-  end
-
-  assign shaf_rready = update_w_from_fifo;
-
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) hash_done <= 1'b0;
-    else         hash_done <= hash_done_next;
-  end
-
-  typedef enum logic [1:0] {
-    FifoIdle,
-    FifoLoadFromFifo,
-    FifoWait
-  } fifoctl_state_e;
-
-  fifoctl_state_e fifo_st_q, fifo_st_d;
-
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      fifo_st_q <= FifoIdle;
-    end else begin
-      fifo_st_q <= fifo_st_d;
-    end
-  end
-
-  always_comb begin
-    fifo_st_d = FifoIdle;
-    update_w_from_fifo = 1'b0;
-    hash_done_next = 1'b0;
-
-    unique case (fifo_st_q)
-      FifoIdle: begin
-        if (hash_start) begin
-          fifo_st_d = FifoLoadFromFifo;
-        end else begin
-          fifo_st_d = FifoIdle;
-        end
-      end
-
-      FifoLoadFromFifo: begin
-        if (!sha_en) begin
-          fifo_st_d = FifoIdle;
-          update_w_from_fifo = 1'b0;
-        end else if (!shaf_rvalid) begin
-          // Wait until it is filled
-          fifo_st_d = FifoLoadFromFifo;
-          update_w_from_fifo = 1'b0;
-        end else if (w_index == 4'd 15) begin
-          fifo_st_d = FifoWait;
-          update_w_from_fifo = 1'b1;
-        end else begin
-          fifo_st_d = FifoLoadFromFifo;
-          update_w_from_fifo = 1'b1;
-        end
-      end
-
-      FifoWait: begin
-        // Wait until next fetch begins (begin at round == 48)a
-        if (msg_feed_complete && complete_one_chunk) begin
-          fifo_st_d = FifoIdle;
-
-          hash_done_next = 1'b1;
-        end else if (complete_one_chunk) begin
-          fifo_st_d = FifoLoadFromFifo;
-        end else begin
-          fifo_st_d = FifoWait;
-        end
-      end
-
-      default: begin
-        fifo_st_d = FifoIdle;
-      end
-    endcase
-  end
-
-  // SHA control
-  typedef enum logic [1:0] {
-    ShaIdle,
-    ShaCompress,
-    ShaUpdateDigest
-  } sha_st_t;
-
-  sha_st_t sha_st_q, sha_st_d;
-
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      sha_st_q <= ShaIdle;
-    end else begin
-      sha_st_q <= sha_st_d;
-    end
-  end
-
-  assign clear_digest = hash_start;
-
-  always_comb begin
-    update_digest    = 1'b0;
-    calculate_next_w = 1'b0;
-
-    init_hash        = 1'b0;
-    run_hash         = 1'b0;
-
-    unique case (sha_st_q)
-      ShaIdle: begin
-        if (fifo_st_q == FifoWait) begin
-          init_hash = 1'b1;
-          sha_st_d = ShaCompress;
-        end else begin
-          sha_st_d = ShaIdle;
-        end
-      end
-
-      ShaCompress: begin
-        run_hash = 1'b1;
-
-        if (round < 48) begin
-          calculate_next_w = 1'b1;
-        end
-
-        if (complete_one_chunk) begin
-          sha_st_d = ShaUpdateDigest;
-        end else begin
-          sha_st_d = ShaCompress;
-        end
-      end
-
-      ShaUpdateDigest: begin
-        update_digest = 1'b1;
-        if (fifo_st_q == FifoWait) begin
-          init_hash = 1'b1;
-          sha_st_d = ShaCompress;
-        end else begin
-          sha_st_d = ShaIdle;
-        end
-      end
-
-      default: begin
-        sha_st_d = ShaIdle;
-      end
-    endcase
-  end
-
-  // complete_one_chunk
-  assign complete_one_chunk = (round == 6'd63);
-
-  sha2_pad u_pad (
-    .clk_i,
-    .rst_ni,
-
-    .wipe_secret,
-    .wipe_v,
-
-    .fifo_rvalid,
-    .fifo_rdata,
-    .fifo_rready,
-
-    .shaf_rvalid,
-    .shaf_rdata,
-    .shaf_rready,
-
-    .sha_en,
-    .hash_start,
-    .hash_process,
-    .hash_done,
-
-    .message_length,
-    .msg_feed_complete
-  );
 
 
-endmodule : sha2
+library work;
+	use work.keccak_globals.all;
+	
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+
+
+
+entity keccak is
+  
+  port (
+    clk     : in  std_logic;
+    rst_n   : in  std_logic;
+    start : in std_logic;
+    din     : in  std_logic_vector(63 downto 0);
+    din_valid: in std_logic;
+    buffer_full: out std_logic;
+    last_block: in std_logic;    
+    ready : out std_logic;
+    dout    : out std_logic_vector(63 downto 0);
+    dout_valid : out std_logic);
+
+end keccak;
+
+architecture rtl of keccak is
+
+--components
+
+component keccak_round
+port (
+
+    round_in     : in  k_state;
+    round_constant_signal    : in std_logic_vector(63 downto 0);
+    round_out    : out k_state);
+
+end component;
+
+component keccak_round_constants_gen
+port (
+    round_number: in unsigned(4 downto 0);
+    round_constant_signal_out: out std_logic_vector(63 downto 0));
+ end component;
+
+
+component keccak_buffer 
+  
+  port (
+    clk     : in  std_logic;
+    rst_n   : in  std_logic;    
+    din_buffer_in     : in  std_logic_vector(63 downto 0);
+    din_buffer_in_valid: in std_logic;
+    last_block: in std_logic;
+    din_buffer_full : out std_logic;
+    din_buffer_out    : out std_logic_vector(1023 downto 0);
+    dout_buffer_in : in std_logic_vector(255 downto 0);
+    dout_buffer_out: out std_logic_vector(63 downto 0);
+    dout_buffer_out_valid: out std_logic;
+    ready: in std_logic);
+
+ end component;
+
+  ----------------------------------------------------------------------------
+  -- Internal signal declarations
+  ----------------------------------------------------------------------------
+
+ 
+  signal reg_data,round_in,round_out: k_state;
+  --signal zero_state : k_state;
+  signal reg_data_vector: std_logic_vector (255 downto 0);
+  signal counter_nr_rounds : unsigned(4 downto 0);
+  --signal zero_lane: k_lane;
+  signal din_buffer_full:std_logic;
+  --signal zero_plane: k_plane;
+  signal round_constant_signal: std_logic_vector(63 downto 0);
+  signal din_buffer_out: std_logic_vector(1023 downto 0);
+  signal permutation_computed : std_logic;
+ 
+  
+begin  -- Rtl
+
+-- port map
+
+round_map : keccak_round port map(round_in,round_constant_signal,round_out);
+round_constants_gen: keccak_round_constants_gen port map(counter_nr_rounds,round_constant_signal);
+buffer_in: keccak_buffer port map(clk, rst_n,
+din,din_valid,last_block, 
+din_buffer_full,din_buffer_out,
+reg_data_vector,
+dout,dout_valid,permutation_computed);
+
+-- constants signals
+--zero_lane<= (others =>'0');
+
+--i000: for x in 0 to 4 generate
+--	zero_plane(x)<= zero_lane;
+--end generate;
+
+--i001: for y in 0 to 4 generate
+--	zero_state(y)<= zero_plane;
+--end generate;
+
+--map part of the state to a vector
+i002: for x in 0 to 3 generate
+	i003: for i in 0 to 63 generate
+		reg_data_vector(64*x+i)<= reg_data(0)(x)(i);
+	end generate;
+end generate;
+
+ 
+ 
+ -- state register and counter of the number of rounds
+ 
+  p_main : process (clk, rst_n)
+    
+  begin  -- process p_main
+    if rst_n = '0' then                 -- asynchronous rst_n (active low)
+      --reg_data <= zero_state;
+      		for row in 0 to 4 loop
+			for col in 0 to 4 loop
+				for i in 0 to 63 loop
+					reg_data(row)(col)(i)<='0';
+				end loop;
+			end loop;
+		end loop;
+      counter_nr_rounds <= (others => '0');
+      permutation_computed <='1';
+    elsif clk'event and clk = '1' then  -- rising clk edge
+
+	if (start='1') then
+		--reg_data <= zero_state;
+		for row in 0 to 4 loop
+			for col in 0 to 4 loop
+				for i in 0 to 63 loop
+					reg_data(row)(col)(i)<='0';
+				end loop;
+			end loop;
+		end loop;
+		counter_nr_rounds <= (others => '0');	
+		permutation_computed<='1';		
+	else
+		if(din_buffer_full ='1' and permutation_computed='1') then
+			counter_nr_rounds(4 downto 0)<= (others => '0');
+			counter_nr_rounds(0)<='1';
+			permutation_computed<='0';
+			reg_data<= round_out;
+		else
+			if( counter_nr_rounds < 24 and permutation_computed='0') then			
+				counter_nr_rounds <= counter_nr_rounds + 1;
+				reg_data<= round_out;
+							
+			end if;
+			if( counter_nr_rounds = 23) then
+				permutation_computed<='1';
+				counter_nr_rounds<= (others => '0');
+			end if;
+		end if;
+		
+	end if;
+    end if;
+  end process p_main;
+
+--input mapping
+
+
+--capacity part
+
+	i01: for col in 1 to 4 generate
+		i02: for i in 0 to 63 generate
+			round_in(3)(col)(i)<= reg_data(3)(col)(i);
+
+		
+		end generate;	
+	end generate;
+
+	i03: for col in 0 to 4 generate
+		i04: for i in 0 to 63 generate
+			round_in(4)(col)(i)<= reg_data(4)(col)(i);
+
+		
+		end generate;	
+	end generate;
+--rate part
+i10: for row in 0 to 2 generate
+	i11: for col in 0 to 4 generate
+		i12: for i in 0 to 63 generate
+			round_in(row)(col)(i)<= reg_data(row)(col)(i) xor (din_buffer_out((row*64*5)+(col*64)+i) and (din_buffer_full and permutation_computed));
+		end generate;	
+	end generate;
+end generate;
+
+i13: for i in 0 to 63 generate
+			round_in(3)(0)(i)<= reg_data(3)(0)(i) xor (din_buffer_out((3*64*5)+(0*64)+i) and (din_buffer_full and permutation_computed));
+end generate;	
+
+	
+
+ready<=permutation_computed;
+buffer_full<=din_buffer_full;
+end rtl;
